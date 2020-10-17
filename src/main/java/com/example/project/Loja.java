97eb9c5d86cdea4352ae06d5929a82948001fbe3
Neto Rocha <jose.rocha@dcx.ufpb.br>
2020-10-16 22:02:32 -0300

    Método e testes corrigidos

--- src/main/java/com/example/project/Loja.java
+++ src/main/java/com/example/project/Loja.java
@@ -47,9 +47,55 @@ public class Loja {
         this.endereco = endereco;
     }
 
-    public String dadosLoja() {
-		// Implemente aqui
-		return null;
+    public static boolean isEmpty(String input){
+		if (input == null){
+			return true;
+		}
+		
+		if (input.equals("")){
+			return true;
+		}
+
+		else{
+			return false;
+		}
 	}
+
+    public static String brk = System.lineSeparator();
+
+    public void validarCamposObrigatorio(){
+        
+        if (isEmpty(this.getNomeLoja())){
+			throw new RuntimeException ("O campo nome da loja é obrigatório");
+        }
+        
+        if(isEmpty(this.getCnpj())){
+            throw new RuntimeException("O campo cnpj da loja é obrigatório");
+        }
+
+        if(isEmpty(this.getInscricaoEstadual())){
+            throw new RuntimeException("O campo inscrição estadual da loja é obrigatório");
+        }
+}
+    public String dadosLoja() {
+
+        String _telefone = isEmpty(getTelefone()) ? "" : "Tel " + getTelefone();
+        _telefone = (!_telefone.isEmpty() && !isEmpty(endereco.getCep())) ? " " + _telefone : _telefone;
+		
+		String _observacao = isEmpty(getObservacao()) ? "" : getObservacao();				
+		String _cnpj = "CNPJ: " + getCnpj();
+        String _ie = "IE: " + getInscricaoEstadual();
+        
+
+        String retornoLoja = getNomeLoja() + brk;
+        retornoLoja += endereco.dadosEndereco();
+        retornoLoja += _telefone + brk;
+		retornoLoja += _observacao + brk;
+		retornoLoja += _cnpj + brk;
+        retornoLoja += _ie + brk;   
+        
+        return retornoLoja;
+
+    }
 
 }
--- src/main/java/com/example/project/Venda.java
+++ src/main/java/com/example/project/Venda.java
@@ -1,5 +1,92 @@
 package com.example.project;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Venda {
-    
+    private Loja loja;
+    private String datahora;
+    private int ccf;
+    private int coo;
+
+    public Venda(Loja loja, String datahora, int ccf, int coo) {
+        this.loja = loja;
+        this.datahora = dataAtual();
+        this.ccf = ccf;
+        this.coo = coo;
+    }
+
+    public Venda(Loja loja, String DATAHORA, String CCF_VENDA, String COO_VENDA) {
+	}
+
+	public Loja getLoja() {
+        return this.loja;
+    }
+
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
+
+    public String getDataHora(){
+        return this.datahora;
+    }
+
+    public void setDataHora(String datahora) {
+        this.datahora = datahora;
+    }
+
+    public int getCcf() {
+        return this.ccf;
+    }
+
+    public void setCcf(int ccf) {
+        this.ccf = ccf;
+    }
+
+    public int getCoo() {
+        return this.coo;
+    }
+
+    public void setCoo(int coo){
+        this.coo = coo;
+    }
+
+    public static String dataAtual(){
+        //Data e hora atuais
+        LocalDateTime now = LocalDateTime.now();
+
+        DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataFormatada = formatterData.format(now);
+        DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String horaFormatada = formatterHora.format(now);
+
+        return dataFormatada + " " + horaFormatada + "V";
+    }
+
+    public void verificarCampoObrigatorioVenda(){
+        if(this.loja == null) {
+            throw new RuntimeException("O campo loja deve está preenchida");
+        }
+        if(this.datahora == null){
+            throw new RuntimeException("O campo hora e data são obrigatórios");
+        }
+
+        if(this.ccf == 0) {
+            throw new RuntimeException("O campo CCF é obrigatório");
+        }
+
+        if(this.coo == 0) {
+            throw new RuntimeException("O campo COO é obrigatório");
+        }
+    }
+
+    public String dadosVenda() {
+
+        String _ccf = "CCF:" + this.ccf + " ";
+        String _coo = "COO: " + this.coo;
+
+        return dataAtual()+ _ccf + _coo;
+        
+    }
+
 }
